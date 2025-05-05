5d5c74405de6ee9a6e58a9b6f7a7febea1e8a2bb
eustachekamala <eustachejared25@gmail.com>
2025-05-06 02:36:07 +0300

    added the GlobalHandlerException to catch all the exceptions in our app

---
+++ apps/bs-network-backend/src/main/java/com/eustache/book_network/handler/BusinessErrorCodes.java
@@ -0,0 +1,28 @@
+package com.eustache.book_network.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum BusinessErrorCodes {
+  NO_CODE(0, NOT_IMPLEMENTED, "No code"),
+  INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Incorrect password"),
+  NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "New Password does not match"),
+  ACCOUNT_LOCKED(302, FORBIDDEN, "Account locked"),
+  ACCOUNT_DISABLED(303, FORBIDDEN, "Account disabled"),
+  BAD_CREDENTIALS(304, BAD_REQUEST, "Login and / or password is incorrect"),
+  ;
+    @Getter
+    private final int code;
+    @Getter
+    private final String description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+  BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+    this.code = code;
+    this.description = description;
+    this.httpStatus = httpStatus;
+  }
+}
