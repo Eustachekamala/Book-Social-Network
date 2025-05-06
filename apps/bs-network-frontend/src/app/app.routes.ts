46fedf8003d7c7b3ff4b6905aa72b9fd7fcdd915
eustachekamala <eustachejared25@gmail.com>
2025-05-07 00:19:07 +0300

    added the activation account componnt plus some updates of the styles of login, register component and created the books component that when the user is logged in, he'll be redirect to the books page

--- apps/bs-network-frontend/src/app/app.routes.ts
+++ apps/bs-network-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
+import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
+import {BooksComponent} from "./pages/books/books.component";
 
 export const appRoutes: Route[] = [
   {
@@ -10,5 +12,13 @@ export const appRoutes: Route[] = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent,
+  },
+  {
+    path: 'books',
+    component: BooksComponent,
   }
 ];
---
+++ apps/bs-network-frontend/src/app/pages/books/books.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-books',
+  imports: [CommonModule],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css',
+})
+export class BooksComponent {}
