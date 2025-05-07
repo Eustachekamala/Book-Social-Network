34c4516b407c4b38d3ca165f4cc0a7529f9cdfe9
eustachekamala <eustachejared25@gmail.com>
2025-05-08 00:59:26 +0300

    added some modules

--- apps/bs-network-backend/src/main/java/com/eustache/book_network/auth/AuthentificationService.java
+++ apps/bs-network-backend/src/main/java/com/eustache/book_network/auth/AuthentificationService.java
@@ -15,7 +15,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
